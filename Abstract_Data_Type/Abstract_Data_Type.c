21b4b97f5d9990956993e7d04444dd1b116d0a0f
XU ZAN <zan.xu@hella.com>
2013-11-15 14:51:58 +0800

    創建第一個解決方案/項目 Abstract_Data_Type
    添加所有的.c源文件和.h頭文件，以及項目文件.vcproj和解決方案文件.sln

---
+++ Abstract_Data_Type/Abstract_Data_Type.c
@@ -0,0 +1,10 @@
+/********************************************************************************************
+ * File name	: Abstract_Data_Type.c
+ * Description	: To describe the ADT, expression approach.
+ * Creator		: XU ZAN
+ * Creation date: Fri.	Nov. 15, 2013
+ * Copyright(C)		2013	All rights reserved.
+ *
+ ********************************************************************************************/
+
+#include "Abstract_Data_Type.h"
---
+++ Abstract_Data_Type/Abstract_Data_Type.h
@@ -0,0 +1,15 @@
+/********************************************************************************************
+ * File name	: Abstract_Data_Type.h
+ * Description	: To describe the ADT, expression approach.
+ * Creator		: XU ZAN
+ * Creation date: Fri.	Nov. 15, 2013
+ * Copyright(C)		2013	All rights reserved.
+ *
+ ********************************************************************************************/
+
+#ifndef ABSTRACT_DATA_TYPE_H
+#define ABSTRACT_DATA_TYPE_H
+
+	typedef struct ADT;
+
+#endif	/*  ABSTRACT_DATA_TYPE_H  */
