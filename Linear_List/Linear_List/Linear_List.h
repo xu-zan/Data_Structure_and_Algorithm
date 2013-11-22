e4861c3636341b02d000f75c00050da0fbcf4a85
XU ZAN <zan.xu@hella.com>
2013-11-22 09:21:28 +0800

    創建Linear_List解決方案／項目，研究綫性表Linear_List這種數據結構．

---
+++ Linear_List/Linear_List/Linear_List.h
@@ -0,0 +1,14 @@
+/********************************************************************************************
+ * File name	: Linear_List.h
+ * Description	: To describe the structure of Linear_List
+ * Creator		: XU ZAN
+ * Creation date: Fri.	Nov. 22, 2013
+ * Copyright(C)		2013	All rights reserved.
+ *
+ ********************************************************************************************/
+
+#ifndef LINEAR_LIST_H
+#define LINEAR_LIST_H
+
+
+#endif	/*  LINEAR_LIST_H  */
