b2b7bdbc8381479da06e4d8004c52a6350748bf4
XU ZAN <zan.xu@hella.com>
2013-12-03 10:43:07 +0800

    對順序表這種數據結構及其對應的操作算法,編寫測試用例進行測試驗證.

---
+++ Linear_List/Linear_List/Testing_SeqList.h
@@ -0,0 +1,17 @@
+/*****************************************************************************
+ * File name	: Testing_SeqList.h
+ * Description	: To carry out the testing case for Sequence List structure.
+ * Creator		: XU ZAN
+ * Creation date: Mon.	Dec. 02, 2013
+ * Copyright(C)		2013	All rights reserved.
+ *
+ *****************************************************************************/
+
+#ifndef TESTING_SEQLIST_H
+#define TESTING_SEQLIST_H
+
+	void General_Testing(void);
+
+	int TestingCase_SeqList_Structure(void);
+
+#endif	/*  TESTING_SEQLIST_H  */
