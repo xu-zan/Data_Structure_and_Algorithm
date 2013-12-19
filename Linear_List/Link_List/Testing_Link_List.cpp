7f3f8af45873cbf1a8cdee73ac2996434c882911
XU ZAN <zan.xu@hella.com>
2013-12-19 08:54:25 +0800

    在Linear_List解決方案下創建Link_List項目, 研究鏈表這種數據結構.

---
+++ Linear_List/Link_List/Testing_Link_List.cpp
@@ -0,0 +1,13 @@
+/**************************************************************************
+ * File name	: Testing_Link_List.c
+ * Description	: Design the test case to test the structure of link list
+ *				  and its algorithm.
+ * Creator		: XU ZAN
+ * Creation date: Mon.	Dec. 16, 2013
+ * Copyright(C)		2013	All rights reserved.
+ *
+ **************************************************************************/
+
+#include "Testing_Link_List.h"
+#include "Link_List.h"
+
---
+++ Linear_List/Link_List/Testing_Link_List.h
@@ -0,0 +1,14 @@
+/**************************************************************************
+ * File name	: Testing_Link_List.h
+ * Description	: Design the test case to test the structure of link list
+ *				  and its algorithm.
+ * Creator		: XU ZAN
+ * Creation date: Mon.	Dec. 16, 2013
+ * Copyright(C)		2013	All rights reserved.
+ *
+ **************************************************************************/
+
+#ifndef TESTING_LINK_LIST
+#define TESTING_LINK_LIST
+
+#endif	/*  TESTING_LINK_LIST  */
