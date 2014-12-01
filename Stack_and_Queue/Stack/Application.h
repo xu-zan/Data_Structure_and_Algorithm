5f1b17229c56a73d32b5142f67efb320a6193daa
XU ZAN <zan.xu@hella.com>
2014-12-01 11:03:58 +0800

    Create Stack_and_Queue solution/project, to study the structure & algorithm of Stack_and_Queue.

---
+++ Stack_and_Queue/Stack/Application.h
@@ -0,0 +1,17 @@
+/*****************************************************************************
+ * File name		: Application.h
+ * Description		: Implement the application example of "Stack and Queue"
+ *					  structure and their algorithm.
+ * Creator			: XU ZAN
+ * Creation date	: Sun.	Nov. 23, 2014
+ * Copyright(C)		2014	All rights reserved.
+ *
+ *****************************************************************************/
+
+
+#ifndef APPLICATION_H
+#define APPLICATION_H
+
+	void Application_Demo(void);
+
+#endif	/*  APPLICATION_H  */
