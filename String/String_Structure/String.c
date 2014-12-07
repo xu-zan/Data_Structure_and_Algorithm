9dcb213041d5decde956ed50dbbc27994138f26e
XU ZAN <Zan.Xu@hella.com>
2014-12-07 16:43:14 +0800

    Use NetBeans IDE to create a new project : String.
    To study the "String" data structure and his algorithm.

---
+++ String/String_Structure/String.c
@@ -0,0 +1,10 @@
+/*******************************************************************************
+ * File name        : String.c
+ * Description      : Implement the algorithm of string structure.
+ * Creator          : XU ZAN
+ * Creation date    : Thu.  Nov. 27, 2014
+ * Copyright(C)     2014    All rights reserved.
+ * 
+ *******************************************************************************/
+
+#include "String.h"
---
+++ String/String_Structure/String.h
@@ -0,0 +1,23 @@
+/*******************************************************************************
+ * File name        : String.h
+ * Description      : Define the string structure.
+ * Creator          : XU ZAN
+ * Creation date    : Thu.  Nov. 27, 2014
+ * Copyright(C)     2014    All rights reserved.
+ * 
+ *******************************************************************************/
+
+#ifndef STRING_H
+#define	STRING_H
+
+    #ifdef	__cplusplus
+    extern "C" 
+    {
+    #endif
+
+    #ifdef	__cplusplus
+    }
+    #endif
+
+#endif	/* STRING_H */
+
