07cf8bb19bf83dca7a7bf0ec0c49adb703ac9809
XU ZAN <Zan.Xu@hella.com>
2014-12-07 17:04:51 +0800

    Merge branch 'develop'.

---
+++ String/String_Main.c
@@ -0,0 +1,24 @@
+/*****************************************************************************************************
+ * Project          : String
+ * Description      : Study the structure of "String" and its algorithm.
+ * =================================================================================================== 
+ * File name        : String_Main.c
+ * Description      : This file will implements the main() portal function for current project.
+ * Creator          : XU ZAN
+ * Creation date    : Wed.  Nov. 26, 2014
+ * Copyright(C)     2014    All rights reserved.
+ * 
+ *****************************************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "Test.h"
+
+int main(int argc, char* argv[]) 
+{
+    General_Test_Cases();
+/*****************************/
+    printf("\n");
+    return (EXIT_SUCCESS);
+}
